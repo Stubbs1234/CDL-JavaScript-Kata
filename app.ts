b5ad734e76d2665f732ad47d25270fd4ffd66d5e
Alex Stubbs <alexstubbsduck@me.com>
2024-10-31 17:43:51 +0000

    Created structure for app

---
+++ app.ts
@@ -0,0 +1,22 @@
+type PricingRule = {
+    unitPrice: number;
+    discountQuantity?: number;
+    discountPrice?: number;
+};
+
+const pricingRules = {
+    "A": { unitPrice: 50, discountQuantity: 3, discountPrice: 130 },
+    "B": { unitPrice: 30, discountQuantity: 2, discountPrice: 45 },
+    "C": { unitPrice: 20 },
+    "D": { unitPrice: 15 }
+};
+
+class Checkout {
+    private basket: { [sku: string]: number } = {};
+    private pricingRules: { [sku: string]: PricingRule };
+
+    constructor(pricingRules: { [sku: string]: PricingRule }) {
+        this.pricingRules = pricingRules;
+    }
+
+}
